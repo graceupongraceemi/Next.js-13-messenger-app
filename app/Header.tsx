b2cf7aad43622a9e23dadaafb605dfb54eb7cdb2
Emiko Esther S <graceupongraceemi@gmail.com>
2023-01-06 12:25:50 -0800

    next.conf.js modified with domains. In Header.jsx of app, Image and text header added with styling

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='flex flex-col items-center space-y-5'>
+        <div className='flex space-x-2 items-center'>
+          <Image
+            src='https://links.papareact.com/jne'
+            height={10}
+            width={50}
+            alt='Logo'
+          />
+
+          <p className='text-blue-400'>Welcome to Meta Messenger</p>
+        </div>
+
+        <Link
+          href='/auth/signin'
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        >
+          Sign In
+        </Link>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
