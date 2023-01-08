4155a526c84b6d5ab5b212d764f3eec6a60c1046
Emiko Esther S <graceupongraceemi@gmail.com>
2023-01-07 16:21:12 -0800

    In Header.tsx, logged in session added in Header function as anoter header section for user profile picture image and user name

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,6 +2,29 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 function Header() {
+  const session = true;
+
+  if (session)
+    return (
+      <header className='sticky top-0 z-50 bg-white flex justify-center items-center p-10 shadow-sm'>
+        <div className='flex space-x-2'>
+          {/* user avatar or profile picture should replace below image */}
+          <Image
+            className='rounded-full mx-2 object-contain'
+            height={10}
+            width={50}
+            src='https://links.papareact.com/jne'
+            alt='Profile Picture'
+          />
+
+          <div>
+            <p>Logged in as:</p>
+            <p>Emi Esther S</p>
+          </div>
+        </div>
+      </header>
+    );
+
   return (
     <header className='sticky top-0 z-50 bg-white flex justify-center items-center p-10 shadow-sm'>
       <div className='flex flex-col items-center space-y-5'>
