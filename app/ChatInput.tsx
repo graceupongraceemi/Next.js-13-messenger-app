33965e10bfe34a09326a6b3a6974fe378c6f1ec1
Emiko Esther S <graceupongraceemi@gmail.com>
2023-01-08 00:12:10 -0800

    ChatInput.tsx and MessageList.tsx files created and imported to page.tsx of app folder. ChatInput.tsx file modified with form div and input and button, and styled

---
+++ app/ChatInput.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ChatInput() {
+  return (
+    <form className='flex'>
+      <input
+        type='text'
+        placeholder='Enter message here...'
+        className='flex-1 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed'
+      />
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed'>
+        Send
+      </button>
+    </form>
+  );
+}
+
+export default ChatInput;
